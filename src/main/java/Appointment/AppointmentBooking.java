69b8019875f688c8afb18578ed4a0d69d0aab6af
gururaj <gururaj.bm@costrategix.com>
2018-04-19 23:55:08 +0530

    TDD appointment booking

--- src/main/java/Appointment/AppointmentBooking.java
+++ src/main/java/Appointment/AppointmentBooking.java
@@ -1,8 +1,66 @@
 package Appointment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AppointmentBooking {
 
     public Boolean isFreeSlotExistForBooking(String startDateTime, String endDateTime) {
         return true;
     }
+
+    public Boolean isAppointmentStartTimeLessThanEndTime(String startDateTime, String endDateTime) {
+        try {
+            Date appointmentStartTime = convertStringToDateTime(startDateTime);
+            Date appointmentEndTime = convertStringToDateTime(endDateTime);
+            if (appointmentStartTime.compareTo(appointmentEndTime) < 0) {
+                return true;
+            }
+        } catch (ParseException e) {
+            return false;
+        }
+
+        return false;
+    }
+
+   public Boolean isAppointmentValidDuration(String startDateTime, String endDateTime) {
+       try {
+           Date appointmentStartTime = convertStringToDateTime(startDateTime);
+           Date appointmentEndTime = convertStringToDateTime(endDateTime);
+
+           if (!isAppointmentOnSameDay(appointmentStartTime, appointmentEndTime)) {
+               return false;
+           }
+
+           Long appointmentDuration = getDateDifferenceInSeconds(appointmentStartTime, appointmentEndTime);
+           if (appointmentDuration < 900 || appointmentDuration > 86400) {
+               return false;
+           }
+
+       } catch (ParseException e) {
+           return false;
+       }
+
+       return true;
+   }
+
+   private Long getDateDifferenceInSeconds(Date startDateTime, Date endDateTime) {
+       return (endDateTime.getTime() - startDateTime.getTime())/1000;
+   }
+
+   private Boolean isAppointmentOnSameDay(Date startDateTime, Date endDateTime) {
+       SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
+       return dateFormat.format(startDateTime).equals(dateFormat.format(endDateTime));
+   }
+
+   private Date convertStringToDateTime(String dateTime) throws ParseException {
+       String pattern = "yyyy-mm-dd hh:mm:ss";
+       SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+       try {
+           return simpleDateFormat.parse(dateTime);
+       } catch (ParseException e) {
+           throw e;
+       }
+   }
 }
--- src/test/java/AppointmentBookingTest.java
+++ src/test/java/AppointmentBookingTest.java
@@ -15,23 +15,88 @@ public class AppointmentBookingTest {
 
     private AppointmentBooking appointmentBook;
     private SlotDao slotDao;
+    private List slotList;
+    private Slot slot;
 
     @Before
     public void setUp() {
         appointmentBook = new AppointmentBooking();
         slotDao = mock(SlotDao.class);
+        slotList = new ArrayList();
+        slot = new Slot();
+    }
+
+    private void getListOfFreeSlotsForGivenDates(String[][] slotDates) {
+        for (String[] slotDate : slotDates) {
+            slot.setStartTime(slotDate[0]);
+            slot.setEndTime(slotDate[1]);
+            slot.setCreateUserId(1);
+            slot.setStatus("free");
+            slotList.add(slot);
+        }
+    }
+
+    @Test
+    public void test_appointmentDate_startDateLessThanEndDate_isValid() {
+        Boolean result
+            = appointmentBook.isAppointmentStartTimeLessThanEndTime(
+                "2018-04-18 09:00:00", "2018-04-18 10:00:00"
+            );
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void test_appointmentDate_startDateGreaterThanEndDate_isInvalid() {
+        Boolean result
+                = appointmentBook.isAppointmentStartTimeLessThanEndTime(
+                "2018-04-18 10:00:00", "2018-04-18 09:00:00"
+        );
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void test_appointmentDate_startDateEndDateEmptyString_isInvalid() {
+        Boolean result
+                = appointmentBook.isAppointmentStartTimeLessThanEndTime(
+                "", ""
+        );
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void test_appointmentDate_diffStartDateEndDate_lessThanFifteenMin_isInvalid() {
+        Boolean result
+                = appointmentBook.isAppointmentValidDuration(
+                "2018-04-18 10:00:00", "2018-04-18 10:05:00"
+        );
+        Assert.assertEquals(false, result);
+    }
+
+    @Test
+    public void test_appointmentDate_diffStartDateEndDate_ofOneHour_isValid() {
+        Boolean result
+                = appointmentBook.isAppointmentValidDuration(
+                "2018-04-18 10:00:00", "2018-04-18 11:00:00"
+        );
+        Assert.assertEquals(true, result);
+    }
+
+    @Test
+    public void test_appointmentDate_isOnDifferentDay_isInvalid() {
+        Boolean result
+                = appointmentBook.isAppointmentValidDuration(
+                "2018-04-18 10:00:00", "2019-04-19 11:00:00"
+        );
+        Assert.assertEquals(false, result);
     }
 
     @Test
     public void testAppointmentGet_betweenRange() {
-        List slotList = new ArrayList();
-        Slot slot = new Slot();
-        slot.setStartTime("2018-04-18 09:00:00");
-        slot.setEndTime("2018-04-18 10:00:00");
-        slot.setCreateUserId(1);
-        slot.setStatus("free");
-        slotList.add(slot);
-
+        String[][] slotDates = {
+            {"2018-04-18 09:00:00","2018-04-18 10:00:00"},
+            {"2018-04-18 10:01:00","2018-04-18 11:00:00"}
+        };
+        getListOfFreeSlotsForGivenDates(slotDates);
         when(
             slotDao.getSlotsBetweenStartAndEndTime(
                 "2018-04-18 09:00:00",
@@ -39,8 +104,8 @@ public class AppointmentBookingTest {
             )
         ).thenReturn(slotList);
 
-        Boolean result = appointmentBook.isFreeSlotExistForBooking("2018-04-18 09:00:00", "2018-04-18 10:00:00");
+        Boolean result
+            = appointmentBook.isFreeSlotExistForBooking("2018-04-18 09:00:00", "2018-04-18 10:00:00");
         Assert.assertEquals(true, result);
     }
-
 }
